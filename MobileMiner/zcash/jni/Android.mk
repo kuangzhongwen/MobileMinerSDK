38c7605312ac0c4782effd7780bbf6a7b5f18e55
况众文 <kzw@meitu.com>
2018-03-12 12:30:20 +0800

    [update] make 文件更新

--- MobileMiner/zcash/jni/Android.mk
+++ MobileMiner/zcash/jni/Android.mk
@@ -6,8 +6,8 @@ LOCAL_MODULE    := silentarmy
 
 LOCAL_SRC_FILES := blake.c sha256.c Silentarmy.c
 
-LOCAL_LDLIBS:= -llog libGLES_mali.so
+LOCAL_LDLIBS:= -llog -pedantic -Wextra -Wall -Wno-deprecated-declarations -Wno-overlength-strings libGLES_mali.so
 
-LOCAL_CFLAGS += -std=c99
+LOCAL_CFLAGS += -std=gnu99
 
 include $(BUILD_SHARED_LIBRARY)
