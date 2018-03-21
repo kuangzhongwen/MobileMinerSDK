3cda4b986f41a3367b7c36408dfc0b75d1dce0c9
况众文 <kzw@meitu.com>
2018-03-21 17:49:48 +0800

    [update] eth jni code update

---
+++ MobileMiner/eth/jni/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CPP_EXTENSION := .cpp
+LOCAL_MODULE    := eth-dev-core
+LOCAL_SRC_FILES := \
+        ./libdevcore/CommonData.cpp \
+        ./libdevcore/FixedHash.cpp \
+        ./libdevcore/Log.cpp \
+        ./libdevcore/RLP.cpp \
+        ./libdevcore/SHA3.cpp \
+        ./libdevcore/Worker.cpp
+
+LOCAL_C_INCLUDES := ./jni/libdevcore
+
+LOCAL_CFLAGS += -std=c++11 -pthread -fexceptions -frtti
+LOCAL_LDLIBS:= -llog -pedantic -Wextra -Wall -Wno-deprecated-declarations -Wno-overlength-strings
+
+include $(BUILD_STATIC_LIBRARY)
