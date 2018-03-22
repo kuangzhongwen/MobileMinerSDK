1aae8a5ad6b4d20ea21ad06910532c2099e7b47e
况众文 <kzw@meitu.com>
2018-03-22 15:27:47 +0800

    [update] build boost prebuilt static library

--- MobileMiner/eth/jni/Android.mk
+++ MobileMiner/eth/jni/Android.mk
@@ -1,23 +1,141 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_atomic
+LOCAL_SRC_FILES := ./boost/lib/libboost_atomic.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_chrono
+LOCAL_SRC_FILES := ./boost/lib/libboost_chrono.a
+include $(PREBUILT_STATIC_LIBRARY)
 
-LOCAL_CPP_EXTENSION := .cpp
-LOCAL_MODULE    := eth-dev-core
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_container
+LOCAL_SRC_FILES := ./boost/lib/libboost_container.a
+include $(PREBUILT_STATIC_LIBRARY)
 
-LOCAL_SRC_FILES := \
-        ./libdevcore/CommonData.cpp \
-        ./libdevcore/FixedHash.cpp \
-        ./libdevcore/Log.cpp \
-        ./libdevcore/RLP.cpp \
-        ./libdevcore/SHA3.cpp \
-        ./libdevcore/Worker.cpp
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_context
+LOCAL_SRC_FILES := ./boost/lib/libboost_context.a
+include $(PREBUILT_STATIC_LIBRARY)
 
-LOCAL_CFLAGS += -std=c++11 -pthread -fexceptions -frtti
-LOCAL_LDLIBS:= -llog -pedantic -Wextra -Wall -Wno-deprecated-declarations -Wno-overlength-strings
-LOCAL_CFLAGS += -I$(LOCAL_PATH)/boost/include
-LOCAL_LDLIBS += -L$(LOCAL_PATH)/boost/lib/ -lboost_system -lboost_...
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_coroutine
+LOCAL_SRC_FILES := ./boost/lib/libboost_coroutine.a
+include $(PREBUILT_STATIC_LIBRARY)
 
-include $(BUILD_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_date_time
+LOCAL_SRC_FILES := ./boost/lib/libboost_date_time.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_exception
+LOCAL_SRC_FILES := ./boost/lib/libboost_exception.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_filesystem
+LOCAL_SRC_FILES := ./boost/lib/libboost_filesystem.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_graph
+LOCAL_SRC_FILES := ./boost/lib/libboost_graph.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_iostreams
+LOCAL_SRC_FILES := ./boost/lib/libboost_iostreams.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_log
+LOCAL_SRC_FILES := ./boost/lib/libboost_log.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_log_setup
+LOCAL_SRC_FILES := ./boost/lib/libboost_log_setup.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_prg_exec_monitor
+LOCAL_SRC_FILES := ./boost/lib/libboost_prg_exec_monitor.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_program_options
+LOCAL_SRC_FILES := ./boost/lib/libboost_program_options.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_random
+LOCAL_SRC_FILES := ./boost/lib/libboost_random.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_regex
+LOCAL_SRC_FILES := ./boost/lib/libboost_regex.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_serialization
+LOCAL_SRC_FILES := ./boost/lib/libboost_serialization.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_signals
+LOCAL_SRC_FILES := ./boost/lib/libboost_signals.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_stacktrace_basic
+LOCAL_SRC_FILES := ./boost/lib/libboost_stacktrace_basic.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_stacktrace_noop
+LOCAL_SRC_FILES := ./boost/lib/libboost_stacktrace_noop.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_system
+LOCAL_SRC_FILES := ./boost/lib/libboost_system.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_test_exec_monitor
+LOCAL_SRC_FILES := ./boost/lib/libboost_test_exec_monitor.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_thread
+LOCAL_SRC_FILES := ./boost/lib/libboost_thread.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_timer
+LOCAL_SRC_FILES := ./boost/lib/libboost_timer.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_type_erasure
+LOCAL_SRC_FILES := ./boost/lib/libboost_type_erasure.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_unit_test_framework
+LOCAL_SRC_FILES := ./boost/lib/libboost_unit_test_framework.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_wave
+LOCAL_SRC_FILES := ./boost/lib/libboost_wave.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libboost_wserialization
+LOCAL_SRC_FILES := ./boost/lib/libboost_wserialization.a
+include $(PREBUILT_STATIC_LIBRARY)
--- MobileMiner/eth/jni/Application.mk
+++ MobileMiner/eth/jni/Application.mk
@@ -1,4 +1,5 @@
 NDK_TOOLCHAIN_VERSION:=4.9
 
-APP_ABI := armeabi
+APP_ABI := all
 APP_STL := gnustl_static
+APP_PLATFORM := android-14
