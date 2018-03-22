933baba9eb1bca377e6e09c33900dabea4728432
况众文 <kzw@meitu.com>
2018-03-22 17:13:49 +0800

    [update] update eth ndk

--- MobileMiner/eth/jni/Android.mk
+++ MobileMiner/eth/jni/Android.mk
@@ -1,15 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CPP_EXTENSION := .cpp
+LOCAL_MODULE := libboost_system
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/boost/include
+LOCAL_SRC_FILES := $(LOCAL_PATH)/boost/lib/libboost_system.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE    := eth-dev-core
 
-LOCAL_C_INCLUDES := ./jni/boost/include
-
-LOCAL_CFLAGS += -std=c++11 -pthread -fexceptions -frtti
-LOCAL_LDLIBS:= -llog -pedantic -Wextra -Wall -Wno-deprecated-declarations -Wno-overlength-strings
-LOCAL_CFLAGS += -I$(LOCAL_PATH)/boost/include
-LOCAL_LDLIBS += -L$(LOCAL_PATH)/boost/lib/ -lboost_system -lboost_...
+LOCAL_CPPFLAGS += -std=c++11 -fexceptions -frtti
+LOCAL_C_INCLUDES += ./jni/boost/include/
+LOCAL_STATIC_LIBRARIES := libboost_system
 
 LOCAL_SRC_FILES := \
         ./libdevcore/CommonData.cpp \
@@ -19,13 +21,6 @@ LOCAL_SRC_FILES := \
         ./libdevcore/SHA3.cpp \
         ./libdevcore/Worker.cpp
 
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE    := eth-miner
-LOCAL_SRC_FILES := EthMiner.cpp
-LOCAL_STATIC_LIBRARIES := eth-dev-core
 include $(BUILD_SHARED_LIBRARY)
-
 
 
