fe7242fe19225f4c90b6f55f0a1bf3967380af81
况众文 <kzw@meitu.com>
2018-03-22 14:00:47 +0800

    [update] code clean

--- MobileMiner/eth/jni/Android.mk
+++ MobileMiner/eth/jni/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_CPP_EXTENSION := .cpp
 LOCAL_MODULE    := eth-dev-core
+
 LOCAL_SRC_FILES := \
         ./libdevcore/CommonData.cpp \
         ./libdevcore/FixedHash.cpp \
@@ -12,11 +13,7 @@ LOCAL_SRC_FILES := \
         ./libdevcore/SHA3.cpp \
         ./libdevcore/Worker.cpp
 
-LOCAL_C_INCLUDES := ./jni/libdevcore
-
 LOCAL_CFLAGS += -std=c++11 -pthread -fexceptions -frtti
 LOCAL_LDLIBS:= -llog -pedantic -Wextra -Wall -Wno-deprecated-declarations -Wno-overlength-strings
-LOCAL_CFLAGS += -I$(LOCAL_PATH)/boost/include/boost-1_49
-LOCAL_LDLIBS += -L$(LOCAL_PATH)/boost/lib/ -lboost_system -lboost_...
 
 include $(BUILD_STATIC_LIBRARY)
