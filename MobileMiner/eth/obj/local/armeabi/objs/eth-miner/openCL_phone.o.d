bf1dfc7698be77b6b9481f9796529f73686b7c13
况众文 <kzw@meitu.com>
2018-03-23 10:37:06 +0800

    [update] git clean cache

---
+++ MobileMiner/eth/obj/local/armeabi/objs/eth-miner/openCL_phone.o.d
@@ -0,0 +1,11 @@
+/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/obj/local/armeabi/objs/eth-miner/./openCL_phone.o: \
+ /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/./openCL_phone.cpp \
+ /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/CL/cl.h \
+ /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/CL/cl_platform.h \
+ /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/./openCL_phone.h
+
+/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/CL/cl.h:
+
+/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/CL/cl_platform.h:
+
+/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/eth/jni/./openCL_phone.h:
