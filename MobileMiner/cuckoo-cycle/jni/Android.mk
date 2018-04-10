12795e539919246419eb6a72a0865c7555a68445
况众文 <kzw@meitu.com>
2018-04-10 15:48:22 +0800

    [update] cuckoo cycle opencl

--- MobileMiner/cuckoo-cycle/jni/Android.mk
+++ MobileMiner/cuckoo-cycle/jni/Android.mk
@@ -4,6 +4,15 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libblake2b
 LOCAL_SRC_FILES := blake2b-ref.c
-LOCAL_CFLAGS += -std=gnu99
+LOCAL_CFLAGS    += -std=gnu99
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libopencl
+LOCAL_CFLAGS    += -std=gnu99
+LOCAL_SRC_FILES := openCL_phone.c
+
+include $(BUILD_STATIC_LIBRARY)
