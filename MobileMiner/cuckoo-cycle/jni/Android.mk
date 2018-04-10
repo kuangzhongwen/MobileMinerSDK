a5f73901734c20dbeeadd5d018d8a5a5862005ee
况众文 <kzw@meitu.com>
2018-04-10 15:45:13 +0800

    [update] cuckoo cycle blake2b

--- MobileMiner/cuckoo-cycle/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libblake2b
-LOCAL_CFLAGS    += -std=gnu99
-LOCAL_SRC_FILES := black2b-ref.c
-
-include $(BUILD_SHARED_LIBRARY)
