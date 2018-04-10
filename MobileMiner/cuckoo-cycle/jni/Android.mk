1a0a6131f6dfa06883376fb41d81100bcaf56c9d
况众文 <kzw@meitu.com>
2018-04-10 11:32:27 +0800

    [update] update cuckoo cycle Android.mk

--- MobileMiner/cuckoo-cycle/jni/Android.mk
+++ MobileMiner/cuckoo-cycle/jni/Android.mk
@@ -2,9 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := zcash-miner
+LOCAL_MODULE    := cuckoo-cycle
 
-LOCAL_SRC_FILES := openCL_phone.c char_utils.c blake.c sha256.c zcash_miner.c
+LOCAL_SRC_FILES := cuckoo.c
 LOCAL_CFLAGS += -std=gnu99
 LOCAL_LDLIBS:= -llog -pedantic -Wextra -Wall -Wno-deprecated-declarations -Wno-overlength-strings
 
