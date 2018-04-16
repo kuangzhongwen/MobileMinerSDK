4020e6165cb0b3aff88b208b2c41ce278ea0545c
况众文 <kzw@meitu.com>
2018-04-16 15:35:40 +0800

    [update] monero cpu jni uv

--- MobileMiner/monero/jni/Android.mk
+++ MobileMiner/monero/jni/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := libcpuid
-LOCAL_C_INCLUDES  += ./jni/3rdparty/libcpuid
+LOCAL_MODULE    := lib-cpuid
+LOCAL_C_INCLUDES  += $(LOCAL_PATH)/3rdparty/libcpuid
 
 LOCAL_SRC_FILES    := \
                     ./3rdparty/libcpuid/cpuid_main.c \
@@ -11,5 +11,28 @@ LOCAL_SRC_FILES    := \
                     ./3rdparty/libcpuid/recog_intel.c \
                     ./3rdparty/libcpuid/libcpuid_util.c
 LOCAL_CFLAGS  += -std=gnu99
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := lib-uv
+LOCAL_EXPORT_C_INCLUDES    := $(LOCAL_PATH)/3rdparty/uv
+LOCAL_LDLIBS    :=  $(LOCAL_PATH)/3rdparty/uv/libuv.so
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := lib-api
+LOCAL_CPPFLAGS := -Wall -std=c++11
+LOCAL_C_INCLUDES    := $(LOCAL_PATH)/3rdparty/rapidjson \
+                       $(LOCAL_PATH)/3rdparty/rapidjson/error \
+                       $(LOCAL_PATH)/3rdparty/rapidjson/internal \
+                       $(LOCAL_PATH)/3rdparty/rapidjson/msinttypes
+LOCAL_SRC_FILES := \
+    ./api/Api.cpp \
+    ./api/ApiState.cpp \
+    ./api/Httpd.cpp \
+    ./api/NetworkState.cpp
+
+LOCAL_STATIC_LIBRARIES := lib-uv
 
 include $(BUILD_STATIC_LIBRARY)
--- MobileMiner/monero/jni/Application.mk
+++ MobileMiner/monero/jni/Application.mk
@@ -1,5 +1,5 @@
 NDK_TOOLCHAIN_VERSION:=4.9
 
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi
 APP_STL := gnustl_static
 APP_PLATFORM := android-14
