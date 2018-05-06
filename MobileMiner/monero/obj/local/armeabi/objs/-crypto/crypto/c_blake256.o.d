5f06cf7631734843fd25199ee3c171712167aead
况众文 <kzw@meitu.com>
2018-05-06 23:23:36 +0800

    [update] clean git cache

--- MobileMiner/monero/obj/local/armeabi/objs/-crypto/crypto/c_blake256.o.d
+++
@@ -1,5 +0,0 @@
-/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/monero/obj/local/armeabi/objs/-crypto/./crypto/c_blake256.o: \
- /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/monero/jni/./crypto/c_blake256.c \
- /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/monero/jni/./crypto/c_blake256.h
-
-/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/monero/jni/./crypto/c_blake256.h:
--- MobileMiner/monero/obj/local/armeabi/objs/-crypto/crypto/c_keccak.o.d
+++
@@ -1,2 +0,0 @@
-/Users/kuangzhongwen/Desktop/project/mining/MobileMiner/monero/obj/local/armeabi/objs/-crypto/./crypto/c_keccak.o: \
- /Users/kuangzhongwen/Desktop/project/mining/MobileMiner/monero/jni/./crypto/c_keccak.c
