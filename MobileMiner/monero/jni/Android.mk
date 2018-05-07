aa57a8b278a5bb46e1810b0f630c45938792525e
况众文 <kzw@meitu.com>
2018-05-07 17:20:21 +0800

    [update] xmr jni microhttpd

--- MobileMiner/monero/jni/Android.mk
+++ MobileMiner/monero/jni/Android.mk
@@ -17,7 +17,39 @@ include $(BUILD_STATIC_LIBRARY)
 
 # uv
 include $(CLEAR_VARS)
+
 LOCAL_MODULE    := lib-uv
 LOCAL_EXPORT_C_INCLUDES    := $(LOCAL_PATH)/3rdparty/uv
 LOCAL_SRC_FILES    := $(LOCAL_PATH)/3rdparty/uv/libuv.a
 include $(PREBUILT_STATIC_LIBRARY)
+
+
+# microhttpd
+include $(CLEAR_VARS)
+
+LIB_ROOT_ABS :=  $(LOCAL_PATH)/3rdparty/libmicrohttpd
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+
+LOCAL_MODULE := lib-microhttpd
+
+LOCAL_SRC_FILES := \
+ $(LIB_ROOT_ABS)/src/microhttpd/base64.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/basicauth.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/connection.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/daemon.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/digestauth.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/internal.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/md5.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/memorypool.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/postprocessor.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/reason_phrase.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/response.c \
+ $(LIB_ROOT_ABS)/src/microhttpd/tsearch.c
+
+LOCAL_C_INCLUDES += \
+ $(LIB_ROOT_ABS) \
+ $(LIB_ROOT_ABS)/src/include \
+ $(LIB_ROOT_ABS)/src/microhttpd
+
+LOCAL_CFLAGS  += -std=gnu99
+include $(BUILD_STATIC_LIBRARY)
