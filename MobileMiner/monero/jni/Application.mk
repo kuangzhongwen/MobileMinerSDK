e3f95f2d15081cdb6370546f9682d4a044c46238
况众文 <kzw@meitu.com>
2018-05-07 22:36:31 +0800

    [update] xmr jni upgrade 3

--- MobileMiner/monero/jni/Application.mk
+++ MobileMiner/monero/jni/Application.mk
@@ -2,6 +2,6 @@ NDK_TOOLCHAIN_VERSION:=4.9
 
 APP_ABI := armeabi armeabi-v7a
 APP_STL := gnustl_static
-APP_PLATFORM := android-14
+APP_PLATFORM :=  android-17
 APP_CPPFLAGS += -fexceptions -fpermissive
 
